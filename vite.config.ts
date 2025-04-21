21f80a8e691f55aaa379d517cc1e3c6c8ec05291
yelbenitez <yelbenitez5@gmail.com>
2025-04-21 20:52:05 +0800

    dev: cleaning up & debugging

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/yelbenitez-github-io.onrender.com',
   // server: {
   //   proxy: {
   //     '/api': {
